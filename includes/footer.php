cb79a8597fe2b939070dd4db27b383e83909ac69
Ubuntu <ubuntu@ip-172-31-18-31.ec2.internal>
2025-05-14 13:44:06 +0000

    Initial commit from EC2

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<div class="footer-section footer-section1">
+<div class="footer-brand">
+    <a href="index.php">
+        <h1 class="footer-heading">Yash's Cinema</h1>
+    </a> 
+</div>    
+</div>
+<div class="footer-section footer-section2">
+    <h2><i class="fas fa-user-alt"></i> Social Media</h2>
+    <div class="footer-section-inner-container">
+     <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+        <span><i class="fab fa-lg fa-facebook-square"></i> Facebook</span>
+    </a>
+    <a href="https://www.x.com" target="_blank" rel="noopener noreferrer">
+        <span><i class="fab fa-lg fa-twitter-square"></i> Twitter</span>
+    </a>
+    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+        <span><i class="fab fa-lg fa-instagram"></i> Instagram</span>
+    </a>
+    </div>
+</div>
+<div class="footer-section  footer-section3">
+    <p>© 2025 Yash's Cinema. Created by Yash Shindey </p>
+    <a>Advertising</a>
+    <a>Privacy Policy</a>
+    <a href="contact-us.php">Contact</a>
+</div>
+
+<!-- <footer class="bg-dark d-flex justify-content-center">
+    <h2 class="text-white" href="#">COVID-19 LIVE STATS</h2>
+    <h2 style="color:#fff;">Developed By <a target="_blank" class="navbar-brand line text-white" href="https://github.com/Yash-Shindey/">Yash Shindey</a></h2>
+</footer> -->
